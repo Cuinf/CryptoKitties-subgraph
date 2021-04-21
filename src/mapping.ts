fb90183f41975b2726c95137b34beb47d5b0f9ce
Cui Qin <qincuidhu@gmail.com>
2021-04-21 20:02:03 +0200

    change to load method

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,17 @@
 import { Birth, Transfer } from '../generated/CryptoKitties/CryptoKitties'
 import { KittyOwner } from '../generated/schema'
+//import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleBirth(event: Birth): void {
     let kitty = new KittyOwner(event.params.kittyId.toHex())
     kitty.owner = event.params.owner
+    //kitty.count = BigInt.fromI32(1)
     kitty.save()
 }
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
-    let kitty = new KittyOwner(id)
+    let kitty = KittyOwner.load(id)
     if (kitty == null) {
         kitty = new KittyOwner(id)
     }
