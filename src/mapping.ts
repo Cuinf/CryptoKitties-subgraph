9cd555b7e4cdb40f9c71714e27835695e2373b91
Cui Qin <qincuidhu@gmail.com>
2021-04-21 18:48:49 +0200

    change to the right id

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,14 +2,14 @@ import { Birth, Transfer } from '../generated/CryptoKitties/CryptoKitties'
 import { KittyOwner } from '../generated/schema'
 
 export function handleBirth(event: Birth): void {
-    let kitty = new KittyOwner(event.params.id.toHex())
+    let kitty = new KittyOwner(event.params.kittyId.toHex())
     kitty.kittyId = event.params.kittyId
     kitty.owner = event.params.owner
     kitty.save()
 }
 
 export function handleTransfer(event: Transfer): void {
-    let id = event.params.id.toHex()
+    let id = event.params.tokenId.toHex()
     let kitty = new KittyOwner(id)
     if (kitty == null) {
         kitty = new KittyOwner(id)
